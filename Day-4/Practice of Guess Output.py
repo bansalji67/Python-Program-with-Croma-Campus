7b5c0ea8e9ab24a06a46659f0d4be5560a4384ca
bansalji67 <sumit.bansal221@gmail.com>
2026-04-26 15:57:44 +0530

    updated

---
+++ Day-4/Practice of Guess Output.py
@@ -0,0 +1,124 @@
+
+"""
+Q-1 What is the output of the following code?
+
+ans- a = 5 
+     b = 10 
+     print(a < b < 20)  # a<b and b<20
+
+Output: True     
+
+
+Q-2 Predict the output:
+
+Ans- x = True 
+     y = False 
+     print(x + y * x) #(1+0*1)
+     
+Output: 1
+
+Q-3 What does this expression evaluate to?
+
+Ans- print(4 ** 0 ** 2) # (4**0)**2=>1**2=>1
+      4 ki power 0 and output's power is 2
+
+Output: 1
+
+Q-4 Find the result of this bitwise operation:
+
+Ans- a = 12   
+     b = 5    
+     print(a ^ b)
+
+    # 12=> 8,4,2,1 binary value is   1100
+    # 5 => 8,4,2,1   binary value is 0101
+    # ^ =>                           1001=> 9
+    
+Q- 5 Guess the output: 
+
+Ans- print((3 and 0) or (0 and 3)) # ((1 and 0)) or (0 and 3))
+
+Output--0
+
+Q-6 What's the output of this tricky comparison?
+
+Ans- print(256 is 256) 
+     print(257 is 257)
+     
+Output: True
+
+Q-7 Evaluate this expression:
+
+Ans a = 7 
+    print(~a + 1)
+    
+Output: -7
+
+Q- 8 What will this print?
+
+Ans a = True 
+    b = False
+
+   print((a or b) and not (a and b))
+
+Output:  True
+
+Q-9 What's the output?
+
+print(10 > 5 == True) # 10 is greater than 5 condition is true
+
+Output: False
+
+Q-10  Evaluate this expression:
+
+Ans- print(2 + 3 * 4 ** 2 // 8 % 3) 
+
+step-1 # 4 is power of 2=16
+step-2 # 2+3 *16 //8 % 3
+step-3 # 3* 16          =48
+step-4 # 2+48 //8 % 3   
+step-5 # 48//8          =6
+step-6 # 2 + 6 % 3      =0
+step-7 # 2+0            =2
+
+Output: 2
+
+Q-11 What does this expression evaluate to?
+
+Ans- print(1 << 2 + 1) # 2+1=3
+                       # (1<<3)
+                       # 1=0001
+                       # 2=0100
+                       # 3=1000
+                       # 8 8421  
+
+Output: 8
+
+Q-12 Predict the output:
+
+Ans- a = 3 
+     b = 2 
+     a *= b + 1 
+     print(a) # a*=3 2+1 3*3
+
+Output: 9
+
+Q-13 Evaluate this chained comparison:
+
+Ans print(3 < 5 > 2 == 2) # 3<5--True
+                          # 5>2--True
+                          # 2==2--True
+                        
+Output: True
+
+Q-14 Guess the result:
+
+
+"""
+print(10 // 3 * 3 + 1 % 3) 
+
+
+
+ 
+
+
